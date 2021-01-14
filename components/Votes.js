a3524bbb14fe49dc219d4e914a336c66a80dbb10
안상현 <ansanghyeon@ansanghyeon-ui-MacBookAir.local>
2021-01-14 10:12:23 +0900

    0114

---
+++ components/Votes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.Text`
+  color: rgb(220, 220, 220);
+  margin-bottom: 7px;
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+const Votes = ({ votes }) => <Container>🌟 {votes} / 10</Container>;
+
+export default Votes;
---
+++ screens/Movies/MoviesPresenter.js
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import { ActivityIndicator, ScrollView, Dimensions } from "react-native";
+import Slide from "../../components/Movies/Slide";
+import Title from "../../components/Title";
+import Vertical from "../../components/Vertical";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+const SliderContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${HEIGHT / 4}px;
+  margin-bottom: 40px;
+`;
+
+const Container = styled.View``;
+
+export default ({ loading, nowPlaying, popular }) => (
+  <ScrollView
+    style={
+      {
+        //backgroundColor: "black",
+      }
+    }
+    contentContainerStyle={{
+      backgroundColor: "black",
+      flex: 1,
+      justifyContent: loading ? "center" : "flex-start",
+    }}
+  >
+    {loading ? (
+      <ActivityIndicator color="white" size="small" />
+    ) : (
+      <>
+        <SliderContainer>
+          <Swiper controlsEnabled={false} loop timeout={3}>
+            {nowPlaying.map((movie) => (
+              <Slide
+                key={movie.id}
+                id={movie.id}
+                //original_title로 가져오면 해당 언어 그대로 옴 => title로 변경
+                title={movie.title}
+                overview={movie.overview}
+                votes={movie.vote_average}
+                backgroundImage={movie.backdrop_path}
+                poster={movie.poster_path}
+              />
+            ))}
+          </Swiper>
+        </SliderContainer>
+        <Container>
+          <Title title={"Popular Movies"} />
+          <ScrollView
+            style={{ maringTop: 20 }}
+            contentContainerStyle={{ paddingLeft: 30 }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {popular.map((movie) => (
+              <Vertical
+                key={movie.id}
+                poster={movie.poster_path}
+                //original_title로 가져오면 해당 언어 그대로 옴 => title로 변경
+                title={movie.title}
+                votes={movie.vote_average}
+              />
+            ))}
+          </ScrollView>
+        </Container>
+      </>
+    )}
+  </ScrollView>
+);
