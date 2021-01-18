40a625b842ff15453f927f2ca69c75d16444e243
sanghyun0127 <ash0127@khu.ac.kr>
2021-01-18 16:54:32 +0900

    react, react-native upgrade

--- components/Detail/Link.js
+++ components/Detail/Link.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { FontAwesome } from "@expo/vector-icons";
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 const Container = styled.View`
   flex-direction: row;
